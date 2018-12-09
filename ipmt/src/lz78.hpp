7273966ef92149a44e354c4f1aaaaee54c786852
Julia Feitosa <mjfc@cin.ufpe.br>
2018-12-09 07:42:35 -0800

    Fixed lz78 class, now the code is a private attribute

--- ipmt/src/lz78.hpp
+++ ipmt/src/lz78.hpp
@@ -26,7 +26,8 @@
 
 class LZ78 {
 
-
+    private:
+        std::vector < std::pair < int, char >> code;
     public:
     /**
     * @brief Construct a new LZ78 object
@@ -37,12 +38,19 @@ class LZ78 {
 
     ~LZ78() {};
 
-    void WriteToFile(const std::string & filename, std::vector < std::pair < int, char >> code) {
+    void PrintDict(){
+        for(int i=0; i<code.size(); i++){
+            std::cout << "<" << code[i].first << "," << code[i].second << "> \n";
+        }
+    }
+
+
+    void WriteToFile(const std::string & filename) {
 
         std::ofstream out(filename.c_str());
-        size_t dict_size = code.size();
+        int dict_size = code.size();
 
-        for (size_t i = 0; i < dict_size; ++i) {
+        for (int i = 0; i < dict_size; ++i) {
             out.write(reinterpret_cast <
                 const char * > ( & code[i].first), sizeof(int));
             out.write( & code[i].second, sizeof(char));
@@ -50,14 +58,14 @@ class LZ78 {
 
     }
 
-    std::string LZ78_Decompress(const std::vector < std::pair < int, char >> & code) {
+    std::string LZ78_Decompress() {
         std::string txt, nextWord;
         std::unordered_map < int, std::string > dict;
         int d = 1;
 
         dict[0] = "";
 
-        for (size_t idx = 0; idx < code.size(); ++idx) {
+        for (int idx = 0; idx < code.size(); ++idx) {
             int dict_index = code[idx].first;
             char decoded_char = code[idx].second;
 
@@ -69,8 +77,7 @@ class LZ78 {
         return txt;
     }
 
-    void Decompress(const std::string & input_filename,
-        const std::string & output_filename) {
+    void Decompress(const std::string & input_filename, const std::string & output_filename) {
 
         std::ifstream in (input_filename);
         std::ofstream out(output_filename);
@@ -86,11 +93,11 @@ class LZ78 {
             in .read(reinterpret_cast < char * > ( & idx), sizeof(int)); in .read( & chr, sizeof(char));
             code.push_back(std::make_pair(idx, chr));
         }
-        txt = LZ78_Decompress(code);
+        txt = LZ78_Decompress();
         out << txt;
     }
 
-    void LZ78_Compress(const std::string & txt, std::vector < std::pair < int, char >> * code) {
+    void LZ78_Compress(const std::string & txt) {
 
         std::unordered_map < std::string, int > dict;
         std::string preffix;
@@ -103,12 +110,13 @@ class LZ78 {
             if (dict.find(preffix + txt[i]) != dict.end()) {
                 preffix += txt[i];
             } else {
-                code->push_back(std::make_pair(dict[preffix], txt[i]));
+                code.push_back(std::make_pair(dict[preffix], txt[i]));
                 dict[preffix + txt[i]] = idx++;
                 preffix.clear();
             }
 
         }
+
 
     }
 
@@ -124,8 +132,8 @@ class LZ78 {
             txt += "\n";
         } in .close();
 
-        LZ78_Compress(txt, & code);
-        WriteToFile(output_filename, code);
+        LZ78_Compress(txt);
+        WriteToFile(output_filename);
 
     }
 
@@ -137,7 +145,7 @@ class LZ78 {
 
     }
 
-    private:
+
 
 };
 
