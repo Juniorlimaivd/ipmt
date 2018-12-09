471fed92ca12b04e52b6916c083cb4f8ec673254
Julia Feitosa <mjfc@cin.ufpe.br>
2018-12-09 07:22:46 -0800

    Prettyfied formatting

--- ipmt/src/lz78.hpp
+++ ipmt/src/lz78.hpp
@@ -12,156 +12,132 @@
 #ifndef LZ78_H
 #define LZ78_H
 
-
-#include <string>
-#include <vector>
-#include <string.h>
-#include <stdexcept>
-#include <iostream>
-#include <fstream>
+#include <string> 
+#include <vector> 
+#include <string.h> 
+#include <stdexcept> 
+#include <iostream> 
+#include <fstream> 
 #include <unordered_map>
 
 #define COMPRESSED_QUALIFIER "_compressed"
 #define UNCOMPRESSED_QUALIFIER "_uncompressed"
 #define EXT ".idx"
 
-
 class LZ78 {
+
+
+    public:
     /**
-     * @brief Size of text chunk
-     * 
-     */
+    * @brief Construct a new LZ78 object
+    * 
+    */
+    
+    LZ78() {};
 
-public:
-    /**
-     * @brief Construct a new LZ78 obidxect
-     * 
-     */
-    LZ78(){};
+    ~LZ78() {};
 
-    /**
-     * @brief Construct a new LZ78 obidxect and encode the parameterstd::string
-     * 
-     * @param txt String to be compressed
-     */
-    LZ78(const std::string &txt, const std::string &output_filename){
-    };
+    void WriteToFile(const std::string & filename, std::vector < std::pair < int, char >> code) {
 
-    /**
-     * @brief Destroy the LZ78 obidxect
-     * 
-     */
-    ~LZ78(){       
-    }; 
+        std::ofstream out(filename.c_str());
+        size_t dict_size = code.size();
 
-    void WriteToFile(const std::string &filename, std::vector<std::pair<int, char>> code){
-
-      std::ofstream out(filename.c_str());
-      size_t dict_size = code.size();
-
-      for(size_t i = 0; i < dict_size; ++i) {
-        out.write(reinterpret_cast<const char*>(&code[i].first), sizeof(int));
-        out.write(&code[i].second, sizeof(char));
-      }
-
-}
-
-    std::string LZ78_Decompress(const std::vector<std::pair<int, char>> &code) {
-      std::string txt, nextWord;
-      std::unordered_map<int, std::string> dict;
-      int d = 1;
-
-      dict[0] = "";
-
-      for(size_t idx = 0; idx < code.size(); ++idx) {
-        int dict_index = code[idx].first;
-        char decoded_char = code[idx].second;
-
-        nextWord = dict[dict_index] + decoded_char;
-        dict[d++] = nextWord;
-        txt += nextWord;
-      }
-
-      return txt;
-    }
-
-    void LZ78_Compress(const std::string &txt, std::vector<std::pair<int, char>> *code) {
-
-      std::unordered_map<std::string, int> dict;
-      std::string preffix;
-      int idx = 1;
-      
-      dict[""] = 0;
-
-      for(int i= 0; i < txt.size(); ++i) {
-
-        if (dict.find(preffix + txt[i]) != dict.end()) {
-          preffix += txt[i];
-        } else {
-          code->push_back(std::make_pair(dict[preffix], txt[i]));
-          dict[preffix + txt[i]] = idx++;
-          preffix.clear();
+        for (size_t i = 0; i < dict_size; ++i) {
+            out.write(reinterpret_cast <
+                const char * > ( & code[i].first), sizeof(int));
+            out.write( & code[i].second, sizeof(char));
         }
 
-
     }
 
+    std::string LZ78_Decompress(const std::vector < std::pair < int, char >> & code) {
+        std::string txt, nextWord;
+        std::unordered_map < int, std::string > dict;
+        int d = 1;
+
+        dict[0] = "";
+
+        for (size_t idx = 0; idx < code.size(); ++idx) {
+            int dict_index = code[idx].first;
+            char decoded_char = code[idx].second;
+
+            nextWord = dict[dict_index] + decoded_char;
+            dict[d++] = nextWord;
+            txt += nextWord;
+        }
+
+        return txt;
     }
 
-    void Compress(const std::string &input_filename, const std::string &output_filename)  {
+    void Decompress(const std::string & input_filename,
+        const std::string & output_filename) {
 
-      std::ifstream in(input_filename.c_str());
-      std::string line, txt;
-      std::vector<std::pair<int, char>> code;
+        std::ifstream in (input_filename);
+        std::ofstream out(output_filename);
 
-      while(getline(in, line))
-      {
-        txt += line;
-        txt += "\n";
-      }
-      in.close();
-      
-      LZ78_Compress(txt, &code);
-      WriteToFile(output_filename, code);
+        std::string txt;
+        int dict_size, idx;
+        char chr;
 
+        std::vector < std::pair < int, char >> code;
 
-    }
+        while (! in .eof()) {
 
-
-
-    void Decompress(const std::string &input_filename, const std::string &output_filename)  {
-
-      std::ifstream in(input_filename);
-      std::ofstream out(output_filename);
-
-     std::string txt;
-      int dict_size, idx;
-      char chr;
-
-
-      std::vector<std::pair<int, char>> code;
-
-      while(!in.eof()) {       
-
-        in.read(reinterpret_cast<char*>(&idx), sizeof(int));
-        in.read(&chr, sizeof(char));
-        code.push_back(std::make_pair(idx, chr));
-      }
+            in .read(reinterpret_cast < char * > ( & idx), sizeof(int)); in .read( & chr, sizeof(char));
+            code.push_back(std::make_pair(idx, chr));
+        }
         txt = LZ78_Decompress(code);
         out << txt;
     }
 
-    std::string ParseFileName(std::string filename,std::string qualifier){
+    void LZ78_Compress(const std::string & txt, std::vector < std::pair < int, char >> * code) {
 
-    
-      filename = filename.substr(0, filename.size()-4);
-      filename = filename + qualifier + EXT;
-      return filename;
+        std::unordered_map < std::string, int > dict;
+        std::string preffix;
+        int idx = 1;
+
+        dict[""] = 0;
+
+        for (int i = 0; i < txt.size(); ++i) {
+
+            if (dict.find(preffix + txt[i]) != dict.end()) {
+                preffix += txt[i];
+            } else {
+                code->push_back(std::make_pair(dict[preffix], txt[i]));
+                dict[preffix + txt[i]] = idx++;
+                preffix.clear();
+            }
+
+        }
 
     }
 
+    void Compress(const std::string & input_filename,
+        const std::string & output_filename) {
 
-private:
+        std::ifstream in (input_filename.c_str());
+        std::string line, txt;
+        std::vector < std::pair < int, char >> code;
+
+        while (getline( in , line)) {
+            txt += line;
+            txt += "\n";
+        } in .close();
+
+        LZ78_Compress(txt, & code);
+        WriteToFile(output_filename, code);
+
+    }
+
+    std::string ParseFileName(std::string filename, std::string qualifier) {
+
+        filename = filename.substr(0, filename.size() - 4);
+        filename = filename + qualifier + EXT;
+        return filename;
+
+    }
+
+    private:
 
 };
 
